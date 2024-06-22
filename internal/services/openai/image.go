8d37d396223d0ce6f38e6073d327153bf8bb79eb
rendizi <rendizi_off@gmail.com>
2024-06-22 16:47:37 +0500

    Init: SummarizeImage - from image to description

---
+++ internal/services/openai/image.go
@@ -0,0 +1,56 @@
+package openai
+
+import(
+	"github.com/go-resty/resty/v2"
+	"encoding/json"
+	"os"
+	"fmt"
+)
+
+func SummarizeImage(url string, prompt string)(string, error){
+	apiEndpoint := "https://api.openai.com/v1/chat/completions"
+
+	apiKey := os.Getenv("OPENAI_KEY")
+    client := resty.New()
+
+    response, err := client.R().
+        SetAuthToken(apiKey).
+        SetHeader("Content-Type", "application/json").
+        SetBody(map[string]interface{}{
+			"model": "gpt-4o",
+			"messages": []interface{}{
+				map[string]interface{}{
+					"role": "user",
+					"content": []interface{}{
+						map[string]interface{}{
+							"type": "text",
+							"text": prompt,
+						},
+						map[string]interface{}{
+							"type": "image_url",
+							"image_url": map[string]interface{}{
+								"url": url,
+							},
+						},
+					},
+				},
+			},
+			"max_tokens": 300,
+		}).
+        Post(apiEndpoint)
+
+    if err != nil {
+        return "",fmt.Errorf("Error while sending send the request: %v", err)
+    }
+
+    body := response.Body()
+
+    var data map[string]interface{}
+    err = json.Unmarshal(body, &data)
+    if err != nil {
+        return "",fmt.Errorf("Error while decoding JSON response:", err)
+    }
+
+    content := data["choices"].([]interface{})[0].(map[string]interface{})["message"].(map[string]interface{})["content"].(string)
+    return content, nil 
+}
